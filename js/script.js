82c493ee3eb5b6882abe6f84a228d123794383ce
fikriwado <mfikri.khoirurrizal.com>
2024-10-23 06:51:27 +0700

    add slider featured in about page

--- js/script.js
+++ js/script.js
@@ -21,56 +21,20 @@ document.addEventListener('DOMContentLoaded', function () {
 $(document).ready(function () {
   if ($('.partner-slider').length) {
     $('.partner-slider').slick({
-      slidesToShow: 5,
+      slidesToShow: 1,
+      slidesToScroll: 1,
       centerMode: false,
       centerPadding: '24px',
-      slidesToScroll: 1,
       autoplay: true,
       infinite: true,
       variableWidth: true,
       dots: false,
       arrows: false,
       touchMove: true,
-      touchThreshold: 5,
+      touchThreshold: 100,
       autoplaySpeed: 5000,
       pauseOnHover: true,
-      speed: 1000,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 5
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
+      speed: 1000
     })
   }
 })
